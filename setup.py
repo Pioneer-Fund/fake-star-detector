5efa98e2f59bfc58621ede7c0f395e01eb2bc6c8
Eric Norman <eric@norman.zone>
2024-02-23 18:57:27 -0800

    Use postgres

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ setup(
     packages=find_packages(exclude=["fake_star_detector_tests"]),
     # package data paths are relative to the package key
     package_data={
-        "fake_star_detector": ["../" + path for path in glob.glob("dbt_project/**", recursive=True)]
+        "fake_star_detector": [
+            "../" + path for path in glob.glob("dbt_project/**", recursive=True)
+        ]
     },
     install_requires=[
         "dagster",
@@ -22,6 +24,7 @@ setup(
         "jupytext",
         "dbt-core",
         "dbt-bigquery",
+        "dbt-postgres",
         # packaging v22 has build compatibility issues with dbt as of 2022-12-07
         "packaging<22.0",
     ],
